0c2b1057ec8961d01d65a362dcb710a0b863b8fc
Niranjan Swami <niranjanswami0411@gmail.com>
2026-03-14 13:35:35 +0530

    Refine comments in Abstract_factory.cpp
    Updated comments to clarify the structure of Product and Factory classes.

--- Factory_Design/Abstract_factory.cpp
+++ Factory_Design/Abstract_factory.cpp
@@ -3,9 +3,9 @@
   then we use this Designing Pattern */ 
 
 /*
-->ProductA and ProductB will become abstract class 
-->ProductAa and ProductAb is a ProductA 
-->ProductBa and ProductBb is a ProductB 
+-> ProductA and ProductB will become abstract class 
+-> ProductAa and ProductAb is a ProductA 
+-> ProductBa and ProductBb is a ProductB 
 
 Similarly there will a Factory abstract class and Mutliple child factory 
 
