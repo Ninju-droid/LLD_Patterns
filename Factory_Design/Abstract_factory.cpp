8bfdd6a43ab7d6a2894d71a8df32825114e7b762
Niranjan <niranjanswami0411@gmail.com>
2026-03-14 13:34:40 +0530

    adding Abstracte Factory

---
+++ Factory_Design/Abstract_factory.cpp
@@ -0,0 +1,12 @@
+/*If there Exists More than One type of products
+  and Multiple factories for Manufacturing them 
+  then we use this Designing Pattern */ 
+
+/*
+->ProductA and ProductB will become abstract class 
+->ProductAa and ProductAb is a ProductA 
+->ProductBa and ProductBb is a ProductB 
+
+Similarly there will a Factory abstract class and Mutliple child factory 
+
+*/
